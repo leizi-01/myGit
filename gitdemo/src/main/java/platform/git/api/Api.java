2df415f7807cab9ddf21df6abbe31c37f2ff677d
leizi-01 <3074499628@qq.com>
2019-03-17 10:59:53 +0800

    thri Commit pom.xml

---
+++ gitdemo/src/main/java/platform/git/api/Api.java
@@ -0,0 +1,7 @@
+package platform.git.api;
+
+/**
+ * Created by hc on 2019-03-17.
+ */
+public class Api {
+}
---
+++ gitdemo/src/main/java/platform/git/config/GitConfig.java
@@ -0,0 +1,7 @@
+package platform.git.config;
+
+/**
+ * Created by hc on 2019-03-17.
+ */
+public class GitConfig {
+}
